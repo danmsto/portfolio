7191dcf7b4f22dd208ffec241a85ef0646cdb214
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2022-12-09 14:24:54 +0000

    experience cards made

--- src/content.js
+++ src/content.js
@@ -1,27 +1,43 @@
+import yorkimage from "./media/universityofyork.png";
+import infineumimage from "./media/infineum.png";
+import eyupimage from "./media/eyup.png";
+
+
 const content = {
   siteHeader: "Daniel M Stoddart",
-  siteSubheader: "EyUp Skills Coding Student • University of York Chemistry Graduate",
-  homeHeading: "Greetings, and welcome to my portfolio",
-  homeSubheading: "I am currently enrolled in the EyUp Skills Full Stack Developer Bootcamp, I am looking for work",
-  homeBody: "This site provides you with an overview of projects I have done both individually and as part of a team",
+  siteSubheader: "EyUp Skills Developer Academy • Chemistry at The University of York",
+  homeHeading: "Former Industrial Research & Development Chemist pursuing a career in Software Development",
+  homeSubheading: "I am finishing the EyUp Skills Developer Academy Full Stack Bootcamp in December 2022",
+  homeBody: "Welcome to my Portfolio site, which has been built using React Bootstrap and Javascript, enjoy!",
   aboutTitle: "About Me",
-  aboutPersonal: "I am from Sheffield",
-  experienceIntro: "I have experience in the below:",
+  aboutPersonal: `I grew up in Sheffield and attended the University of York and completed a Masters Degree in Chemistry.
+     After University I entered the Private Sector in Engine Oil Additives with Infineum UK Ltd in Oxfordshire, a joint venture between Shell and ExxonMobil,
+     before making the decision to change career paths and enter Software Development with a place at the EyUp Skills Developer Academy`,
+  experienceIntro: "Here is a brief overview of my experience",
   experience: [
     {
-      heading: "EyUp Skills Full Stack Developer Student",
+      heading: "Full Stack Developer Student",
       body: `16 weeks experience with HTML, CSS, JavaScript, React, 
-      React Bootstrap, Java, OOP, Express, Spring Boot, etc.`
+      React Bootstrap, Java, OOP, Express, Spring Boot, etc.`,
+      image: eyupimage,
+      location: "EyUp Skills Ltd",
+      dates: "2022"
     }, {
-      heading: "Research and Development Chemist at Infineum UK",
+      heading: "Industrial R&D Chemist",
       body: `Worked within Product Development and in the Shell International 
       Account, with two internal technical publications, on synergistic 
       interactions of detergents and antioxidant componentry, and on 
-      the industry-standard turbocharger deposits test`
+      the industry-standard turbocharger deposits test`,
+      image: infineumimage,
+      location: "Infineum UK Ltd",
+      dates: "2018-2022"
     }, {
-      heading: "University of York Chemistry Graduate",
+      heading: "MChem Student",
       body: `I completed my MChem project on Fragment Elaboration Synthesis of 
-      Functionalised Piperidines under Prof Peter O'Brien`
+      Functionalised Piperidines under Prof Peter O'Brien`,
+      image: yorkimage,
+      location: "The University of York",
+      dates: "2014-2018"
     }
   ],
   siteFooter: "Last Manual Update 2022-12-02"
--- src/projects.js
+++ src/projects.js
@@ -8,21 +8,21 @@ const projects = [
       "Tracker for Zero-sum 1-v-1 games (for desktop only)",
     link: "https://danmsto.github.io/poolPool/",
     linkText: "Have a Look!",
-    image: collectorImage,
+    image: collectorImage
   },
   {
     title: "'DoughJoe Pizza'",
     description: "A mock pizza vendor website, made alongside the rest of my EyUp Skills cohort",
     link: "https://danmsto.github.io/doughjoe-pizza",
     linkText: "Hungry?",
-    image: pizzaImage,
+    image: pizzaImage
   },
   {
     title: "project 3",
     description: "description 3",
     link: "https://danmsto.github.io/pizza",
     linkText: "three(3)",
-    image: collectorImage,
+    image: "holder.js/100px180"
   },
 ];
 
