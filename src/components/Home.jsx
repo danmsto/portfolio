e4fe8b576778ba8cdab68b3bbc49e9952ccf3211
Daniel Stoddart <112119554+danmsto@users.noreply.github.com>
2023-02-21 05:37:40 +0000

    home heading centre-aligned (#12)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
 
   return (
     <Container className="mt-1 p-0">
-      <h3>{content.homeHeading}</h3>
+      <h3 className="homeHeading">{content.homeHeading}</h3>
       <p>{content.homeBody}</p>
     </Container>
   );
